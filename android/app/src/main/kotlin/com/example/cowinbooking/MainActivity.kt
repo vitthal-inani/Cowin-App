ec49de1c856270fb03fe6da91b7177a3eba37810
unknown <vitthal.inani@gmail.com>
2021-05-26 12:58:10 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/cowinbooking/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cowinbooking
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Screens/OTPScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class OTPScreen extends StatefulWidget {
+  const OTPScreen({Key? key}) : super(key: key);
+
+  @override
+  _OTPScreenState createState() => _OTPScreenState();
+}
+
+class _OTPScreenState extends State<OTPScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [],
+    );
+  }
+}
