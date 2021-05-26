6961e6df931c65ca414ecc9fa866b7358d5df43a
unknown <vitthal.inani@gmail.com>
2021-05-26 13:48:32 +0530

    OTP

--- lib/Screens/OTPScreen.dart
+++ lib/Screens/OTPScreen.dart
@@ -8,11 +8,50 @@ class OTPScreen extends StatefulWidget {
 }
 
 class _OTPScreenState extends State<OTPScreen> {
+  final GlobalKey _form = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [],
+    final _size = MediaQuery.of(context).size;
+    return Container(
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          SizedBox(
+            width: _size.width * 0.8,
+            child: Form(
+              child: TextFormField(
+                decoration: InputDecoration(
+                    helperText: "Phone Number",
+                    prefixIcon: Text("+91"),
+                    suffix: IconButton(
+                      icon: Icon(Icons.arrow_forward_sharp),
+                      onPressed: () {},
+                    )),
+              ),
+            ),
+          ),
+          SizedBox(
+            width: _size.width * 0.8,
+            child: Form(
+              child: TextFormField(
+                decoration: InputDecoration(
+                  helperText: "Phone Number",
+                  prefixIcon: Container(
+                      padding: EdgeInsets.all(8),
+                      margin: EdgeInsets.only(right: 5),
+                      // alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 1),
+                      ),
+                      child: Text("+91")),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
+import 'package:cowinbooking/Screens/OTPScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-
+        body: OTPScreen(),
       ),
     );
   }
